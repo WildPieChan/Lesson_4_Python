a9072ba249fa6ebdf95d5e2c397aac52a537d5e8
Kuksa <kirkfanton.kb@gmail.com>
2022-11-15 17:41:29 +0300

    Fourth task complete

--- Program_4.py
+++ Program_4.py
@@ -3,31 +3,30 @@ import random
 def NumbersForEquation(quantity):
     array = [random.randint(0, 21) for i in range(quantity + 1)]
     return array
-    
-def EquationCreate(sp):
-    lst= sp[::-1]
-    wr = ''
-    if len(lst) < 1:
-        wr = 'x = 0'
-    else:
-        for i in range(len(lst)):
-            if i != len(lst) - 1 and lst[i] != 0 and i != len(lst) - 2:
-                wr += f'{lst[i]}x^{len(lst)-i-1}'
-                if lst[i+1] != 0:
-                    wr += ' + '
-            elif i == len(lst) - 2 and lst[i] != 0:
-                wr += f'{lst[i]}x'
-                if lst[i+1] != 0:
-                    wr += ' + '
-            elif i == len(lst) - 1 and lst[i] != 0:
-                wr += f'{lst[i]} = 0'
-            elif i == len(lst) - 1 and lst[i] == 0:
-                wr += ' = 0'
-    return wr
 
-def AnswerToFile(st):
+def EquationCreate(numbersArray):
+    result = ''
+    if len(numbersArray) < 1:
+        result = 'x = 0'
+    else:
+        for i in range(len(numbersArray)):
+            if i != len(numbersArray) - 1 and numbersArray[i] != 0 and i != len(numbersArray) - 2:
+                result += f'{numbersArray[i]}x^{len(numbersArray) - i - 1}'
+                if numbersArray[i + 1] != 0:
+                    result += ' + '
+            elif i == len(numbersArray) - 2 and numbersArray[i] != 0:
+                result += f'{numbersArray[i]}x'
+                if numbersArray[i + 1] != 0:
+                    result += ' + '
+            elif i == len(numbersArray) - 1 and numbersArray[i] != 0:
+                result += f'{numbersArray[i]} = 0'
+            elif i == len(numbersArray) - 1 and numbersArray[i] == 0:
+                result += ' = 0'
+    return result
+
+def AnswerToFile(result):
     with open('file.txt', 'w') as data:
-        data.write(st)
+        data.write(result)
 
 k = int(input("Enter the natural degree: k = "))
 ratio = NumbersForEquation(k)
